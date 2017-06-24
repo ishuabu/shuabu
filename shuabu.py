ad878dddfcabbb701819542318d0050162bcb1f3
ishuabu <33000295@qq.com>
2017-06-24 14:33:50 +0800

    Update shuabu.py

--- shuabu.py
+++ shuabu.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 #date: 刷微信步数.
+#host www.ishuabu.com
 #usage: edit steps and ledongli's uid(u need to download this app) .That would be ok .Good luck. ^_^
 import requests
 import sys
